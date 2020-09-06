813bf71dee39a6e4a7e6a8dd7beed528c035991b
teodutu <teodor.dutu@gmail.com>
2020-09-06 14:12:05 +0300

    [UPB][Lab 5] Eliminare cod neutilizat

--- Laboratoare/UPB/Laborator5/desmitm.py
+++ Laboratoare/UPB/Laborator5/desmitm.py
@@ -1,84 +1,13 @@
-import sys
-import random
-import string
 from operator import itemgetter
-import time
-import bisect
+from bisect import bisect_left
 from Crypto.Cipher import DES
-
-def strxor(a, b): # xor two strings (trims the longer input)
-	return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b)])
-
-
-def hexxor(a, b): # xor two hex strings (trims the longer input)
-	ha = a.decode('hex')
-	hb = b.decode('hex')
-	return "".join([chr(ord(x) ^ ord(y)).encode('hex') for (x, y) in zip(ha, hb)])
-
-
-def bitxor(a, b): # xor two bit strings (trims the longer input)
-	return "".join([str(int(x)^int(y)) for (x, y) in zip(a, b)])
-
-
-def str2bin(ss):
-	"""
-	Transform a string (e.g. 'Hello') into a string of bits
-	"""
-	bs = ''
-	for c in ss:
-		bs = bs + bin(ord(c))[2:].zfill(8)
-	return bs
-
-
-def str2int(ss):
-	"""
-	Transform a string (e.g. 'Hello') into a (long) integer by converting first
-	to a bistream
-	"""
-	bs = str2bin(ss)
-	li = int(bs, 2)
-	return li
-
-
-def hex2bin(hs):
-	"""
-	Transform a hex string (e.g. 'a2') into a string of bits (e.g.10100010)
-	"""
-	bs = ''
-	for c in hs:
-		bs = bs + bin(int(c,16))[2:].zfill(4)
-	return bs
-
-
-def bin2hex(bs):
-	"""
-	Transform a bit string into a hex string
-	"""
-	bv = int(bs,2)
-	return int2hexstring(bv)
-
-
-def byte2bin(bval):
-	"""
-	Transform a byte (8-bit) value into a bitstring
-	"""
-	return bin(bval)[2:].zfill(8)
-
-
-def int2hexstring(bval):
-	"""
-	Transform an int value into a hexstring (even number of characters)
-	"""
-	hs = hex(bval)[2:]
-	lh = len(hs)
-	return hs.zfill(lh + lh%2)
 
 
 def get_index(a, x):
 	"""
 	Locate the leftmost value exactly equal to x in list a
 	"""
-	i = bisect.bisect_left(a, x)
+	i = bisect_left(a, x)
 	if i != len(a) and a[i] == x:
 		return i
 	else:
